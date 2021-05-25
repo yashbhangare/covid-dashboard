5f8aef429c58c982b4a61011f318615241d74946
Ishaan Bhatia <ishaanbhatiawork@gmail.com>
2021-05-25 22:42:28 +0530

    Jigyarth, Vaishnavi, Aishwarya and Rajat made the Dashboard CSS

--- src/components/hospitals-list/hospitalsList.js
+++ src/components/hospitals-list/hospitalsList.js
@@ -7,10 +7,7 @@ const HospitalsList = () => {
   const [searchHospital, setSearchHospital] = useState("");
 
   return (
-    <div className="hospital-dashboard">
-      <div className="test-message">
-        <b>Note:</b> "hospital" stuff is under test.
-      </div>
+
       <div className="hospitals-list">
         <div className="hospitals-list-header-component">
           <h3 className="hospitals-list-heading">Covid-19 Dashboard</h3>
@@ -36,25 +33,26 @@ const HospitalsList = () => {
           .map((item, key) => {
             return (
               <div key={key} class="no-bullets">
-                <li>Updated Date - {(item.Timestamp).slice(0, 10)}</li>
-                <li>Updated Time - {((item.Timestamp).slice(11, 19))}</li>
-                <li>City - {item.City} </li>
-                <li>Name - {item.Hospital_Name} </li>
-                <li>Address - {item.Hospital_Address}</li>
-                <li>Phone Number - {item.Hospital_Phone_Number}</li>
-                <li>Bed Availability - {item.Bed_Availability}</li>
-                <li>Oxygen Availability - {item.Oxygen_Availability}</li>
-                <li>Ventilator Availability - {item.Ventilator_Availability}</li>
+                 <li><b>{item.Hospital_Name}</b></li>
+                 <li><b>City - </b>{item.City} </li>
+                 <li><b>Address -  </b>{item.Hospital_Address}</li>
+                 <li><b>Phone Number - </b>{item.Hospital_Phone_Number}</li>
+                 <li><b>Bed Availability - </b>{item.Bed_Availability}</li>
+                 <li><b>Oxygen Availability - </b>{item.Oxygen_Availability}</li>
+                 <li><b>Ventilator Availability - </b>{item.Ventilator_Availability}</li>
+                 <li><b>Updated Date - </b>{(item.Timestamp).slice(0, 10)}</li>
+                 <li><b>Updated Time - </b>{((item.Timestamp).slice(11, 19))}</li>
                 <br />
               </div>
             );
           })}
 
         <div />
-        <div className="css-message">Pardon the CSS.</div>
+
       </div>
-    </div>
+
   );
 };
 
 export default HospitalsList;
+
